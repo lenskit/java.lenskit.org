0295e9cde88ec6fca03e831c50ef8aaa70cec126
Michael Ekstrand <michael@elehack.net>
2014-06-27 15:01:33 -0500

    active tabs on upper menu

--- s/lenskit.js
+++ s/lenskit.js
@@ -1,15 +1,23 @@
 $(document).foundation();
 hljs.initHighlighting();
 
-$('.side-nav li').each(function() {
-  var url = $('a', this).attr('href');
+function navMatches(elt) {
+  var path = window.location.pathname;
+  var url = $('a', elt).attr('href');
   if (url === undefined || url.match('^(https?:)?\\/\\/')) {
-    return;
+    return false;
   }
-  var mode = $('a', this).attr('data-match');
-  var active = false;
-  if (mode === 'exact') {
-    active = url === window.location.pathname;
+  var mode = $('a', elt).attr('data-match');
+  if (mode === undefined) {
+    mode = 'prefix';
+  }
+  console.log('checking url ' + url);
+  if (mode == 'exact') {
+    return url === path;
+  } else if (mode.substring(0,3) == 're:') {
+    var regex = mode.substring(3);
+    console.log('testing regex ' + regex);
+    return new RegExp(regex).test(path);
   } else {
     var pat;
     if (url.charAt(0) == '/') {
@@ -18,9 +26,19 @@ $('.side-nav li').each(function() {
       pat = '(^|/)' + url;
     }
     var regex = new RegExp(pat);
-    active = window.location.pathname.match(regex);
+    return path.match(regex);
   }
-  if (active) {
+}
+
+$('.side-nav li').each(function() {
+  if (navMatches(this)) {
+    console.log('found active URL');
+    $(this).addClass('active');
+  }
+});
+$('#site-menu li').each(function() {
+  if (navMatches(this)) {
+    console.log('found active URL');
     $(this).addClass('active');
   }
 });
